52003b4586e5594521af9b6bee09e5b9eaf66898
Fiona Bianchi <fiona@katieandfiona.co.uk>
2014-06-13 20:17:58 +0100

    Adding new clean Ruby project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyWithSwagger::Application.config.secret_token = '75015b3d8eb6c9345188fc4e6507bf6c82bd6196f75f926e21d407621273862367ea13eee56babf042b9a9ef215993f833d9781fc9759418cb78332c4e5cc3ab'
